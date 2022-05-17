b64524f7a5112f5b515b166ef0252708c35c9028
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-17 21:12:26 +0100

    added removePost.js function.

---
+++ functions/fn/Post-functions/createPost.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const db = require("../../services/db");
+const { Post } = require("../../model/post");
+
+exports.createPost = functions.https.onRequest(async (request, response) => {
+  if (request.method == "POST") {
+    const { postedBy, content, category } = request.body;
+    if (postedBy && content && category) {
+      try {
+        const post = new Post(postedBy, content, category);
+        const res = await db.addPostToDb(post);
+        return response.status(200).send(post);
+      } catch (error) {
+        functions.logger.error("Error creating a comment", error);
+        return response.status(500).send(error);
+      }
+    } else {
+      return response.status(500).send("Error creating a Post");
+    }
+  } else {
+    return response.status(501).send("Request method not supported ");
+  }
+});

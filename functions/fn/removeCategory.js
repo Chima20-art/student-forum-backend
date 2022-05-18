bb28c9ece025b2e22b640490772b51eecd978e19
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-18 18:48:30 +0100

    added removeCategory.js function

---
+++ functions/fn/removeCategory.js
@@ -0,0 +1,21 @@
+const functions = require("firebase-functions");
+const db = require("../services/db");
+
+exports.removeCategory = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "DELETE") {
+      const { id } = request.body;
+      if (id) {
+        try {
+          const res = await db.removeCategory(id);
+          return response.status(200).send(res);
+        } catch (error) {
+          functions.logger.error("Post id is not defined", error);
+          return response.status(500).send(error);
+        }
+      }
+    } else {
+      return response.status(501).send("Request method is not defined");
+    }
+  }
+);

11ba124d8dd9c244ad0f9d5993482286d67b26f0
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-19 20:25:57 +0100

    added addPostComment function .

---
+++ functions/fn/Post-functions/addPostComment.js
@@ -0,0 +1,34 @@
+const functions = require("firebase-functions");
+const db = require("../../services/db");
+
+exports.addPostComment = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "POST") {
+      const { commentId, postId } = request.body;
+      if (commentId && postId) {
+        try {
+          const postDoc = await db.getPostById(postId);
+          if (postDoc.exists) {
+            let post = postDoc.data();
+            if (post.comments.includes(commentId)) {
+              post.comments.unshift(commentId);
+              const res = await db.addPostToDb(post);
+              return response.status(200).send(post);
+            } else {
+              return response.status(500).send("Duplicated Comment Id");
+            }
+          } else {
+            return response.status(500).send("Post does not exist");
+          }
+        } catch (error) {
+          functions.logger.error("Can't add a Post comment", error);
+          return response.status(500).send(error);
+        }
+      } else {
+        return response.status(500).send("Can't add a Post comment");
+      }
+    } else {
+      return response.status(501).send("Request method not supported");
+    }
+  }
+);

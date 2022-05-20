9039af1d718feec5952aad6e919a164a0bfe255a
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-20 12:15:55 +0100

    added getPost function.

--- functions/fn/Post-functions/getPost.js
+++ functions/fn/Post-functions/getPost.js
@@ -7,8 +7,20 @@ exports.getPost = functions.https.onRequest(async (request, response) => {
       const { id } = request.body;
       if (id) {
         const postDoc = await db.getPostById(id);
+
         if (postDoc.exists) {
-          return response.status(200).send(postDoc.data());
+          let post = postDoc.data();
+          post.comments = post.comments.map(async (commentId) => {
+            let commentDoc = await db.getCommentById(commentId);
+            if (commentDoc.exists) {
+              const comment = commentDoc.data();
+
+              return comment;
+            } else {
+              return null;
+            }
+          });
+          return response.status(200).send(post);
         } else {
           return response.status(500).send("Post does not exist.");
         }

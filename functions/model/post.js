0a824fa35bd610caa64c30a02dd187e5ad60eec4
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-06-09 23:28:22 +0100

    added title to the post model.

--- functions/model/post.js
+++ functions/model/post.js
@@ -1,11 +1,12 @@
 /* eslint-disable require-jsdoc */
-const uid = require("uid").uid;
+const uid = require('uid').uid;
 
 class Post {
-  constructor(postedBy, content, category) {
+  constructor(postedBy, title, content, category) {
     this.postedBy = postedBy;
     this.content = content;
     this.category = category;
+    this.title = title;
     this.likes = [];
     this.id = uid(32);
     this.createdAt = Date.now();

8ddb561f7cfacc7f7e350bd9fb4998a2343998f1
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-06-09 23:26:08 +0100

    added insertion of comment.

--- functions/fn/Post-functions/addPostComment.js
+++ functions/fn/Post-functions/addPostComment.js
@@ -1,33 +1,32 @@
 const functions = require('firebase-functions');
 const db = require('../../services/db');
+const { Comment } = require('../../model/comment');
 
 exports.addPostComment = functions.https.onRequest(
   async (request, response) => {
     response.set('Access-Control-Allow-Origin', '*');
 
     if (request.method == 'POST') {
-      const { commentId, postId } = JSON.parse(request.body);
-      if (commentId && postId) {
-        try {
+      try {
+        const { postedBy, content, postId } = JSON.parse(request.body);
+        if (postedBy && content && postId) {
           const postDoc = await db.getPostById(postId);
           if (postDoc.exists) {
+            const comment = new Comment(postedBy, content);
+            const commentCraeted = await db.addCommentToDb(comment);
             let post = postDoc.data();
-            if (!post.comments.includes(commentId)) {
-              post.comments.unshift(commentId);
-              const res = await db.addPostToDb(post);
-              return response.status(200).send(post);
-            } else {
-              return response.status(500).send('Duplicated Comment Id');
-            }
+            post.comments.unshift(comment.id);
+            const res = await db.addPostToDb(post);
+            return response.status(200).send('ok');
           } else {
             return response.status(500).send('Post does not exist');
           }
-        } catch (error) {
-          functions.logger.error("Can't add a Post comment", error);
-          return response.status(500).send(error);
+        } else {
+          return response.status(503).send("Can't add a Post comment");
         }
-      } else {
-        return response.status(500).send("Can't add a Post comment");
+      } catch (error) {
+        functions.logger.error("Can't add a Post comment", error);
+        return response.status(502).send('error');
       }
     } else {
       return response.status(501).send('Request method not supported');
